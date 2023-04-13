e53026d6f95a4fb3958cf2df57f547baebe6dc79
airtondias <airtondias2930@gmail.com>
2023-04-12 21:47:51 -0300

    exercicios de operadores

---
+++ script.js
@@ -0,0 +1,16 @@
+let kelvin = (77-32)*(5/9) + 273.15
+console.log (kelvin)
+
+//mostre 80Cº em Fº
+// função de conversao para farenheight
+//farenheight = (GRAUS) * (9/5) + 32
+let farenheight = (80) * (9/5) + 32
+console.log(farenheight + " F")
+
+//mostre 30ºC em Faren e Kelvin
+let kelvin (30-32)*(5/9) + 273.15
+console.log(g_kelvin + "k")
+
+
+//console.log(g_farenheight + " F")
+//console.log(g_kelvin + " K")
