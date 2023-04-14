15621bacb66a127aadf29b91a1aa2977336fa7aa
airtondias <airtondias2930@gmail.com>
2023-04-13 21:57:41 -0300

    aula operadores

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
 let kelvin = (77-32)*(5/9) + 273.15
-console.log (kelvin)
+//console.log (kelvin)
 
 //mostre 80Cº em Fº
 // função de conversao para farenheight
 //farenheight = (GRAUS) * (9/5) + 32
 let farenheight = (80) * (9/5) + 32
-console.log(farenheight + " F")
+//console.log(farenheight + " F")
 
 //mostre 30ºC em Faren e Kelvin
-let kelvin (30-32)*(5/9) + 273.15
-console.log(g_kelvin + "k")
+let celcius = (30-32)*(5/9) + 273.15
+//console.log(g_celcius + "k")
 
 
 //console.log(g_farenheight + " F")
 //console.log(g_kelvin + " K")
+
+
+
+//Preciso solicitar nome do usuario
+//Solicitar informação: prompt
+//Solicitar informação: console.log
+
+let nome = prompt("Digite seu nome:");
+
+//Solicitar 3 notas
+let nota1 = Number(prompt("Digite nota 1:"));
+let nota2 = Number(prompt("Digite nota 2:"));
+let nota3 = Number(prompt("Digite nota 3:"));
+
+
+//Calcular media
+let soma = nota1 + nota2 + nota3;
+let media = soma / 3;
+
+
+let resultado = media >= 6;
+
+console.log(nome, resultado)
+
+alert(nome,', sua media foi ', media + '. Parabens');
+
+
+let Number = prompt ("digite o numero")
+
+var resto = n % 2;
+
+if (resto == 0) {
+    console.log( + " par");
+} else {
+    console.log( + " impar");
+}
+
+let Number = prompt ("digite o numero")
+
+let Number1 = (prompt(digite o Number1));
+let Number2 = (prompt(digite o Number2));
+let maiornumber >
+console={.log}
+
+
+
